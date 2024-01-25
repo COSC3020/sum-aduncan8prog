b5fb87aa809354cc8dfb27fd5dcd28d2f0b65ef8
aduncan8prog <ammon.duncan@gmail.com>
2024-01-24 19:42:02 -0700

    sum function fixed

--- code.js
+++ code.js
@@ -1,7 +1,25 @@
+//I was having issues with determining whether or not I installed all the correct packages
+//So I looked at egkallas's code to see if we had a similarly working function
+
 function sum(a) {
-    var sum = a[0];
-    for(var i = 0; i < a.length; i++) {
-        sum += a[i];
+
+    //initialize sum var
+    var sum = 0
+
+    //if there is no length to array, return 0
+    if(a.length == 0){
+        sum = 0;
     }
+    //if the array length is 1, then the sum is the first value in array
+    else if(a.length == 1){
+        sum = a[0];
+    }
+    //else, take the sum of every element in the array
+    else{
+        for(var i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+    }
+
     return sum;
 }
